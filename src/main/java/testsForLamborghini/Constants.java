d3f6014a6f794aed8c366eae38b2462f661333fc
ArturMadoyan <84032273+ArturMadoyan@users.noreply.github.com>
2021-09-30 17:39:12 +0400

    Update Constants.java

--- src/main/java/testsForLamborghini/Constants.java
+++ src/main/java/testsForLamborghini/Constants.java
@@ -1,5 +1,5 @@
 package testsForLamborghini;
 
 public class Constants {
-    public static final String EXPECTED_TITLE = "(415) 526-7320 Lamborghini Dealer near San Francisco Bay Area 620 Du Bois St San Rafael";
+    public static final String EXPECTED_TITLE = "Lamborghini Beverly Hills | Lamborghini Dealer Los Angeles";
 }
